4784a3aa6b69caae73c67de38c10e7d8cb099cf7
vsaveris <vsaveris@gmail.com>
2020-01-12 03:24:21 +0100

    feat: setup.py file first draft
    First draft of the setup.py file. The package creation tested with the
    Test pyPi.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'trading-technical-indicators',
+    version = '1.0',
+    author= 'Vasileios Saveris',
+    author_email = 'vsaveris@gmail.com',
+    description = 'Trading Technical Indicators, Open Source Library, in Python',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/vsaveris/trading-technical-indicators',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires = '>=3.7',
+)
