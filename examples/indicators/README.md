17894afab26bbdd863a7c21924e83e355f4a8276
vsaveris <vsaveris@gmail.com>
2020-01-26 12:25:14 +0100

    feat: EMA examples added.
    EMA examples added in the repository.

--- examples/indicators/README.md
+++ examples/indicators/README.md
@@ -44,6 +44,45 @@ The script generates the below plots:
 ##### SMA-200 and SMA-50
 ![](./figures/indicators_sma_50_200_example.png?raw=true)
 
+### EMA Technical Indicator example
+```
+$python python indicators_ema.py
+
+- Graph ./figures/indicators_ema_200_example.png saved.
+- Graph ./figures/indicators_ema_50_200_example.png saved.
+
+EMA data:
+                 EMA-26     EMA-200
+Date
+2000-02-01   19.230000   19.230000
+2000-02-02   19.230000   19.230000
+2000-02-03   19.204850   19.206433
+2000-02-04   19.220267   19.220026
+2000-02-07   19.222524   19.222061
+...                ...         ...
+2012-09-06  142.691924  136.832406
+2012-09-07  142.815485  136.907308
+2012-09-10  142.699524  136.950519
+2012-09-11  142.569929  136.990314
+2012-09-12  142.409934  137.024341
+
+[3169 rows x 2 columns]
+
+EMA value at 2012-09-06: [142.6919242663251, 136.83240626411458]
+
+EMA value at 2012-09-12 00:00:00 : [142.40993445468686, 137.02434112063497]
+
+Signal: Hold [ 0 ]
+```
+
+The script generates the below plots:
+
+##### EMA-200
+![](./figures/indicators_ema_200_example.png?raw=true)
+
+##### EMA-200 and EMA-50
+![](./figures/indicators_ema_50_200_example.png?raw=true)
+
 
 ## Prerequisites
 1. python: `version 3.6` or later
---
+++ examples/indicators/indicators_ema.py
@@ -0,0 +1,56 @@
+'''
+File name: indicators_ema.py
+    Example code rlated to the tradingti.indicator package.
+    EMA Trading Indicator.
+           
+Author: Vasileios Saveris
+enail: vsaveris@gmail.com
+
+License: MIT
+
+Date last modified: 26.01.2020
+
+Python Version: 3.6
+'''
+
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+# Converters used in parsing the dates from csv
+register_matplotlib_converters()
+
+from tradingti.indicators import EMA
+import tradingti as tti
+
+# Read data from csv file. Set the index to the correct column (dates column)
+df = pd.read_csv('./data/sample_data.csv', parse_dates = True, index_col = 0)
+
+# Calculate the EMA indicator for span period = 200
+ema = EMA(df[['Adj Close']], span_periods = [200])
+
+# Save the plot of the calculated Technical Indicator
+ema.getTiPlot().savefig('./figures/indicators_ema_200_example.png')
+
+print('- Graph ./figures/indicators_ema_200_example.png saved.')
+
+# Calculate the EMA indicator for the default span periods (short term ema = 26,
+# long term ema = 200) 
+ema = EMA(df[['Adj Close']])
+
+# Save the plot of the calculated Technical Indicator
+ema.getTiPlot().savefig('./figures/indicators_ema_50_200_example.png')
+print('- Graph ./figures/indicators_ema_50_200_example.png saved.')
+
+# Get EMA calculated data
+print('\nEMA data:\n', ema.getTiData())
+
+# Get EMA value for a specific date
+print('\nEMA value at 2012-09-06:', ema.getTiValue('2012-09-06'))
+
+# Get the most recent EMA value
+print('\nEMA value at', df.index[0], ':', ema.getTiValue())
+
+# Get signal from EMA
+signal = ema.getSignal()
+for key, value in tti.TRADE_SIGNALS.items(): 
+    if value == signal:
+        print('\nSignal:', key, '[', value, ']')
